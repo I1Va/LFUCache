6c86c4a83bd6759f25e0c72e95807044a6279be5
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-10-17 23:38:51 +0300

    add Readme

--- main.cpp
+++ main.cpp
@@ -2,8 +2,32 @@
 
 #include "LFUCache.hpp"
 
+int slowGetPage(int key) {
+    std::cout << "cache miss!\n";
+    return 0;
+}
+
 int main() {
-    int a, b;
-    std::cin >> a >> b;
-    std::cout << a + b;
+    int cacheCapacity = 0;
+    std::cout << "Enter cache capacity : ";
+    std::cin >> cacheCapacity;
+    std::cout << "Enter queries (Ctrl+D to end): \n";
+
+    if (cacheCapacity < 0) {
+        std::cout << "cacheCapacity should be > 0";
+        return 1;
+    }
+
+    cache::LFUCache<int> LFUCache(cacheCapacity);
+    
+    int q = 0;
+    int hits = 0;
+    int queries = 0;
+    while (std::cin >> q) {
+        queries++;
+        hits += LFUCache.lookupUpdate(q, slowGetPage);
+    }
+
+    std::cout << "Count of queries    : " << queries << "\n";
+    std::cout << "Count of cache hits : " << hits << "\n";
 }
